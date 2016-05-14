e61172a4f8d5bdb757055d0088071f18c45d3b21
Justin LaRose <justinplarose@gmail.com>
2016-05-14 18:45:15 -0400

    Saturday First Commit
    Added beginning of the region function to grab the country value and pass it to determine the regions.

--- main.js
+++ main.js
@@ -6,8 +6,8 @@
 var countryFunction = function () {
     "use strict";
     
-    console.log(data);
-    console.log("testing");
+    //Below is the format to access the region names inside of the countries
+    console.log(data[0].regions[0].name);
     
     
     var i = 0, elem = document.getElementById("countrySelect");
@@ -18,5 +18,15 @@ var countryFunction = function () {
     }
    
     //elem.innerHTML += "hello test";
+};
+
+var regionFunction = function () {
+    "use strict";
+    //Below is how to pull the value for the country after clicking it
+    var country = document.getElementById("countrySelect").value;
+    //Need to search through the data object to get the index number, then I can list out the regions below it
+    //Need to research how to gray out a form if there are no options for it i.e. Mypos
+    
+    console.log(country);
 };
 
