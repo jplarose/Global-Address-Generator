b3443961691b809ae0791ed3c391b759b2562b45
Justin LaRose <justinplarose@gmail.com>
2016-05-15 23:35:29 -0400

    Added some postal code validation
    still need to complete this portion

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var countryFunction = function () {
     //Below is the format to access the region names inside of the countries
     //console.log(data[0].regions[0].name);
     
-    var i = 0, elem = document.getElementById("countrySelect"); 
+    var i = 0, elem = document.getElementById("countrySelect");
     for (i; i < data.length; i += 1) {
         $(elem).append("<option>" + data[i].country + "</option>");
     }
@@ -15,8 +15,7 @@ var countryFunction = function () {
 var regionFunction = function () {
     "use strict";
     //Below is how to pull the value for the country after clicking it
-    var country = document.getElementById("countrySelect").value, i = 0, r = 0, elem2 = document.getElementById("stateProvinceSelect"), elem3 = document.getElementById("Postal Code");
-    //elem2.options = null;
+    var country = $("#countrySelect")[0].value, i = 0, r = 0, elem2 = $("#stateProvinceSelect")[0], elem3 = $("#PostalCode")[0];
     
     for (i; i < data.length; i += 1) {
         if (country === data[i].country) {
@@ -27,7 +26,7 @@ var regionFunction = function () {
     }
     if (country === "Mypos") {
         //Can get this field to disable, but if information was entered before that it doesn't remove it. I can get around this, but it isn't ideal
-        $(elem3).trigger('reset');
+        //$(elem3).trigger('reset');
         $(elem3).prop('disabled', true);
     } else {
         $(elem3).prop('disabled', false);
--- validation.js
+++ validation.js
@@ -2,14 +2,12 @@
 
 var addressValidation = function () {
     "use strict";
-    var sendToObject = function (key, value) {
-      //Add the Key and Value to the JSON Object to send to the textarea  
-    };
     
     var values = [$("#address1")[0], $("#address2")[0], $("#countrySelect")[0], $("#City")[0], $("#stateProvinceSelect")[0], $("#PostalCode")[0]], i = 0, o = 0, address1Valid, address2Valid, countryValid, cityValid, stateProvinceValid, postalValid;
     var keyValues = ["address1", "address2", "country", "city", "stateProvince", "postal"];
     var validateFlag = [address1Valid, address2Valid, countryValid, cityValid, stateProvinceValid, postalValid];
 
+    //Not-Empty validation check
     for (i; i < values.length; i += 1) {
         if (values[i].value != "") {
             //sendToObject(keyValues[i], values[i]);
@@ -18,14 +16,37 @@ var addressValidation = function () {
             validateFlag[i] = false;
         }
     }
+     
+    //Postal Code validations
+    var postalCodeVal = function () {
+        switch ($("#countrySelect")[0]) {
+        case "Canada":
+                //if statement to determine if it is formatted properly (A1A 1A1)
+            break;
+        case "Mypos":
+            return false;
+        case "United States":
+                //if statement to determine if it is formatted properly (11111)
+            break;
+        default:
+                //if statement to determine if it is formatted properly (alpha-numeric only)
+        }
+    };
+    
+   
+    //Need to get the code for region and country to send to the object instead of the straight value
+    
+    //Flag checking - then error sending or sending the info to the JSON object
+    
+    var sendToObject = function (key, value) {
+      //Add the Key and Value to the JSON Object to send to the textarea  
+    };
+    
+    validateFlag[5] = postalCodeVal();
     
     for (o; o < values.length; o += 1) {
         if (validateFlag[o] == true) {
             console.log(values[o].value);
         }
     }
-    
-    //Need to add specific validations for country, cityProvince, and postal
-    
-    
 };
