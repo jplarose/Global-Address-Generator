3bfd466e377f37826cbfd673b9f8602eed62377c
Justin LaRose <justinplarose@gmail.com>
2016-05-17 23:55:49 -0400

    Completed Functionality
    Completed all validation routines, completed sending the values to a JSON object, send a string of the object to the output area. Did some cleanup on code for consistency.

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
 /*global $ */
 var countryFunction = function () {
     "use strict";
-    //Below is the format to access the region names inside of the countries
-    //console.log(data[0].regions[0].name);
     
     var i = 0, elem = document.getElementById("countrySelect");
     for (i; i < data.length; i += 1) {
         $(elem).append("<option>" + data[i].country + "</option>");
     }
-   
-    //elem.innerHTML += "hello test";
 };
 
 var regionFunction = function () {
     "use strict";
-    //Below is how to pull the value for the country after clicking it
     var country = $("#countrySelect")[0].value, i = 0, r = 0, elem2 = $("#stateProvinceSelect")[0], elem3 = $("#PostalCode")[0];
     
     for (i; i < data.length; i += 1) {
         if (country === data[i].country) {
             var countryIndex = i;
-            //console.log(countryIndex);
             break;
         }
     }
     if (country === "Mypos") {
-        //Can get this field to disable, but if information was entered before that it doesn't remove it. I can get around this, but it isn't ideal
-        //$(elem3).trigger('reset');
+        //I can get this field to disable, but if information was entered before that it doesn't remove it. I can get around this, but it isn't ideal
         $(elem3).prop('disabled', true);
     } else {
         $(elem3).prop('disabled', false);
     }
-    
     if (data[countryIndex].regions.length === 0) {
         $(elem2).empty();
         $(elem2).prop('disabled', true);
@@ -42,7 +34,5 @@ var regionFunction = function () {
             $(elem2).append("<option>" + data[countryIndex].regions[r].name + "</option>");
         }
     }
-    //console.log(country);
-    //console.log(data[1].regions);
 };
 
--- validation.js
+++ validation.js
@@ -3,20 +3,8 @@
 var addressValidation = function () {
     "use strict";
     
-    var values = [$("#address1")[0], $("#address2")[0], $("#countrySelect")[0], $("#City")[0], $("#stateProvinceSelect")[0], $("#PostalCode")[0]], i = 0, o = 0, address1Valid, address2Valid, countryValid, cityValid, stateProvinceValid, postalValid;
-    var keyValues = ["address1", "address2", "country", "city", "stateProvince", "postal"];
-    var validateFlag = [address1Valid, address2Valid, countryValid, cityValid, stateProvinceValid, postalValid];
-
-    //Not-Empty validation check
-    for (i; i < values.length; i += 1) {
-        if (values[i].value != "") {
-            //sendToObject(keyValues[i], values[i]);
-            validateFlag[i] = true;
-        } else {
-            validateFlag[i] = false;
-        }
-    }
-     
+    var postalValid;
+    
     //Postal Code validations
     var postalCodeVal = function () {
         switch ($("#countrySelect")[0].value) {
@@ -51,19 +39,113 @@ var addressValidation = function () {
     };
     
    
-    //Need to get the code for region and country to send to the object instead of the straight value
-    
-    //Flag checking - then error sending or sending the info to the JSON object
-    
-    var sendToObject = function (key, value) {
-      //Add the Key and Value to the JSON Object to send to the textarea  
+    //Getting the Country Code
+    var countryCode = function () {
+        var i;
+        for (i = 0; i < data.length; i += 1) {
+            if ($("#countrySelect")[0].value == data[i].country) {
+                var cCode = data[i].code;
+                return cCode;
+            }
+        }
+    };
+    //Getting the Region Code
+    var regionCode = function () {
+        var u;
+        if ($("#countrySelect")[0].value == "Mypos") {
+            return "";
+        } else {
+            var i;
+            for (i = 0; i < data.length; i += 1) {
+                if ($("#countrySelect")[0].value == data[i].country) {
+                    var countryIndex = i;
+                    for (u = 0; u < data[countryIndex].regions.length; u += 1) {
+                        if ($("#stateProvinceSelect")[0].value == data[countryIndex].regions[u].name) {
+                            return data[countryIndex].regions[u].code;
+                        }
+                    }
+                }
+            }
+        }
+        
     };
     
-    validateFlag[5] = postalCodeVal();
-    
-    for (o; o < values.length; o += 1) {
-        if (validateFlag[o] == true) {
-            console.log(values[o].value);
+    //Flag checking - will return a single true or false that determines if a JSON object is populated
+    var flagCheck = function (address1, country, city, region, postalFlag) {
+        if (country == "Mypos") {
+            $("#address1Error")[0].innerHTML = "";
+            $("#cityError")[0].innerHTML = "";
+            if (address1 != "" && city != "") {
+                return true;
+            } else {
+                if (address1 == "") {
+                    $("#address1Error")[0].innerHTML = "This is a required field";
+                }
+                if (city == "") {
+                    $("#cityError")[0].innerHTML = "This is a required field";
+                }
+                return false;
+            }
+        } else {
+            var gettingTheIndex = function () {
+                var i;
+                for (i = 0; i < data.length; i += 1) {
+                    if (country == data[i].country) {
+                        return i;
+                    }
+                }
+            };
+            var countryIndex = gettingTheIndex();
+            $("#address1Error")[0].innerHTML = "";
+            $("#countryError")[0].innerHTML = "";
+            $("#cityError")[0].innerHTML = "";
+            $("#regionError")[0].innerHTML = "";
+            
+            if (address1 != "" && country != "" && city != "" && postalFlag == true) {
+                if (data[countryIndex].regions.length < 0 && region == "") {
+                    return true;
+                } else if (data[countryIndex].regions.length >= 0 && region != "") {
+                    return true;
+                }
+            } else {
+                if (address1 == "") {
+                    $("#address1Error")[0].innerHTML = "This is a required field";
+                }
+                if (country == "") {
+                    $("#countryError")[0].innerHTML = "This is a required field";
+                }
+                if (city == "") {
+                    $("#cityError")[0].innerHTML = "This is a required field";
+                }
+                if (region == "") {
+                    $("#regionError")[0].innerHTML = "This is a required field";
+                } else {
+                    $("#regionError")[0].innerHTML = "This is a required field";
+                }
+                return false;
+            }
         }
+    };
+    //Creating the JSON Object, populating it and sending it as a string to the output area
+    var sendToObject = function (address1, address2, country, city, region, postal) {
+        var outputObject = new Object();
+        var values = [address1, address2, country, city, region, postal], i;
+        var keyValues = ["address1", "address2", "country", "city", "region", "postal"];
+        
+        for (i = 0; i < values.length; i += 1) {
+            if (values[i] != "") {
+                var name = keyValues[i];
+                outputObject[name] = values[i];
+            }
+        }
+        $("#outputArea")[0].innerHTML = JSON.stringify(outputObject);
+    };
+    
+    postalValid = postalCodeVal();
+    var newCountryCode = countryCode();
+    var newRegionCode = regionCode();
+    var finalFlagCheck = flagCheck($("#address1")[0].value, $("#countrySelect")[0].value, $("#City")[0].value, $("#stateProvinceSelect")[0].value, postalValid);
+    if (finalFlagCheck == true) {
+        sendToObject($("#address1")[0].value, $("#address2")[0].value, newCountryCode, $("#City")[0].value, newRegionCode, $("#PostalCode")[0].value);
     }
 };
